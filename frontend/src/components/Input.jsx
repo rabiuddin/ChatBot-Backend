cf6313a66f19d6e9abd0dbc5e184a78d1d0b7af8
rabiuddin <rabiuddin1@gmail.com>
2025-01-07 17:00:55 +0500

    Remove unused code and update component import in main.jsx

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -13,7 +13,6 @@ export default function Input({ onSend }) {
             onSend(message, true); 
             setIsLoading(true);
             setMessage('');
-            // await new Promise((resolve) => setTimeout(resolve, 5000)); 
             try {
                 const response = await axios.post('http://127.0.0.1:8000/', { prompt: message });
                 onSend(response.data.data, false); 
