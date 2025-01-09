73c99abbff99aa3eb35346d4fe13ff5a732b9500
rabiuddin <rabiuddin1@gmail.com>
2025-01-09 23:06:16 +0500

    Add MergeStack assistant API and integrate with chat completion service

--- src/app/api/chatCompletion/main.py
+++ src/app/api/chatCompletion/main.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-from src.app.helper.gemini_helper import gemini_chat_completion
-from src.app.helper.openai_helper import openai_chat_completion
+from src.app.helper.gemini_helper_folder.gemini_helper import gemini_chat_completion
+from src.app.helper.openai_helper_folder.openai_helper import openai_chat_completion
 from src.app.models.prompt_model import PromptRequest
 from src.app.config import Config
 
@@ -11,13 +10,14 @@ router = APIRouter()
 
 @router.post("/")
 def getResponse(request: PromptRequest):
+    available_models = config.get_available_models()
     try:
         if request.prompt == "":
             return {"data": "Please provide a prompt."}
         elif request.model == "":
             return {"data": "Please provide a model."}
-        elif request.model not in config.get_available_models():
-            return {"data": "The model you provided is not available."}
+        elif request.model not in available_models:
+            return {"data": f"The model you provided is not available. We only have {', '.join(available_models)}"}
         elif request.model == "gpt-4":
             response = openai_chat_completion(request)
         elif request.model == "gemini-1.5-flash":
