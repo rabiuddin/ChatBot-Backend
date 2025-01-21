30c25cdc04be3b206c9b76ccc3e678422aa1f036
rabiuddin <rabiuddin1@gmail.com>
2025-01-21 23:22:04 +0500

    Add LangchainService for chat completion and update requirements

--- src/app/api/chat_completion/chat_completion.py
+++ src/app/api/chat_completion/chat_completion.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, status, Request
 from src.app.helper.gemini.gemini_helper import gemini_chat_completion
-from src.app.helper.openai.openai_helper import openai_chat_completion
 from src.app.models.prompt_model import PromptRequest
 from src.app.config import Config
 from src.app.utils.encryption_utils import decrypt
 from src.app.utils.response_builder_utils import ResponseBuilder
 from src.app.utils.limiter_utils import limiter
+from src.app.helper.langchain.langchain_chat_completion import LangchainService
 
 config = Config()
 
@@ -26,12 +26,15 @@ def getResponse(request: Request, request_body: PromptRequest):
         elif request_body.model not in config.get_openai_allowed_models() and request_body.model not in config.get_gemini_allowed_models():
             return response_builder.set_success(False).set_data(f"The model you provided is not available. We only have {config.get_gemini_allowed_models() + config.get_openai_allowed_models()}").setStatusCode(status.HTTP_400_BAD_REQUEST).build()
         
-        if request_body.model in config.get_openai_allowed_models():
-            response = openai_chat_completion(request_body)
-        elif request_body.model in config.get_gemini_allowed_models():
+        if request_body.model == config.get_gemini_allowed_models()[1]:
             response = gemini_chat_completion(request_body)
+            return response_builder.set_success(True).set_data(response).setStatusCode(status.HTTP_200_OK).build()
+        
+        langchainService = LangchainService()
+        response = langchainService.langchain_chat_completion(request=request_body) 
             
         return response_builder.set_success(True).set_data(response).setStatusCode(status.HTTP_200_OK).build()
     except Exception as e:
-        return response_builder.set_success(False).set_data(f"Sorry Our AI services are currently down, try again later.\nExeption: {e}").setStatusCode(status.HTTP_500_INTERNAL_SERVER_ERROR).build()
+        print(str(e))
+        return response_builder.set_success(False).set_data(f"").setStatusCode(status.HTTP_500_INTERNAL_SERVER_ERROR).set_error(f"Sorry Our AI services are currently down, try again later.\nExeption: {e}").build()
 
