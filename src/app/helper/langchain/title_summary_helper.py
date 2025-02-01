f45675479d6ff4f914bab0eb298b1a0fa9c65a26
rabiuddin <rabiuddin1@gmail.com>
2025-02-01 18:17:56 +0500

    Replace ChatOpenAI with ChatGoogleGenerativeAI in title summary helper and update model parameters

--- src/app/helper/langchain/title_summary_helper.py
+++ src/app/helper/langchain/title_summary_helper.py
@@ -1,19 +1,19 @@
 from src.app.config.config import Config
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
 
 config = Config()
 
 def get_chat_summary(message: str):
     try:
-        model = ChatOpenAI(
-            model='gpt-4',
-            api_key=config.get_openai_api_key(),
+        model = ChatGoogleGenerativeAI(
+            model='gemini-1.5-flash',
+            api_key=config.get_gemini_api_key(),
             max_tokens=config.get_max_tokens(),
             temperature=config.get_model_temperature(),
         )
         messages = [
-            SystemMessage("Based on the following message you have to suggest a title to name the chat / conversation. Provide only one concise title without any quotation marks nothing more nothing less, only plain text:"),
+            SystemMessage("Based on the following message you have to suggest a title to name the chat / conversation. Provide only one concise title without any quotation marks nothing more nothing less, only plain text, but you can add spaces:"),
             HumanMessage(message)
         ]
 
